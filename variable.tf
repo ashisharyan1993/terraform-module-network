c668e655550578d58c53ecb654e2b58e603f338e
Ashish Kumar <35107543+ashisharyan1993@users.noreply.github.com>
2024-07-12 13:16:01 +0530

    Create variable.tf

---
+++ variable.tf
@@ -0,0 +1,14 @@
+variable "vpc_cidr_block" {
+  description = "VPC network"
+}
+variable "public_subnet_cidr_block" {
+  description = "Public Subnet"
+}
+variable "private_subnet_cidr_block" {
+  description = "Private Subnet"
+}
+variable "vpc_prifix" {}
+variable "region" {}
+
+
+
